fbe9bd03215b288cbe70de611483295e2ed49c8a
akarshi19 <akarshigmathur@gmail.com>
2025-12-08 11:00:32 +0530

    Updated changes

--- mobile-app/src/services/supabase.js
+++ mobile-app/src/services/supabase.js
@@ -2,8 +2,8 @@ import 'react-native-url-polyfill/auto';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://tdylzqwlowvniwhvrejh.supabase.co'; // Paste from Step 1.1
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRkeWx6cXdsb3d2bml3aHZyZWpoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ4Mjc1MzUsImV4cCI6MjA4MDQwMzUzNX0.IImaK-DVYiMYaZu4HUOiX9oGhm8zjEXJ4ePDTs9Abyw'; // Paste from Step 1.1
+const supabaseUrl = 'YOUR_SUPABASE_PROJECT_URL';
+const supabaseAnonKey = 'YOUR_SUPABASE_ANON_KEY';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
